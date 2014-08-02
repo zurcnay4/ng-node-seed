5984ce7278b1938649df8d6ea855f9d924890d50
zurcnay4 <zurcnay4@gmail.com>
2014-08-03 01:31:02 +0800

    included express.static to recognize libs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 // configuration =================
 
-//app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 										// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									// parse application/json
